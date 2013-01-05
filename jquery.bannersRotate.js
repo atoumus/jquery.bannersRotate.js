50b36f5501bdeab4ceb596da53d67e4435fbf176
at0m1x <yarutin.kv@gmail.com>
2013-01-05 08:27:42 -0800

    Create jquery.bannersRotate.js

---
+++ jquery.bannersRotate.js
@@ -0,0 +1,36 @@
+(function($){
+    jQuery.fn.bannersRotate = function(){
+        var banners;
+        var timer = null;
+        var currentBannerId = 0;
+        var previousBannerId = null;
+        
+        var make = function(){
+            banners = $(this).children();
+            if(banners.length > 0) {
+                rotate();
+            }
+        };
+        
+        var rotate = function() {
+            if(timer != null) {
+                clearTimeout(timer);
+            }
+            
+            if(previousBannerId != null) {
+                $(banners[previousBannerId]).hide();
+            }
+            
+            currentBanner = $(banners[currentBannerId]);
+            
+            currentBanner.show();
+            
+            timer = setTimeout(function() { rotate() }, parseInt(currentBanner.attr("rel")) * 1000);
+
+            previousBannerId = currentBannerId;
+            currentBannerId = banners.length - 1 == currentBannerId ? 0 : currentBannerId + 1;
+        }
+ 
+        return this.each(make);
+    };
+})(jQuery);
